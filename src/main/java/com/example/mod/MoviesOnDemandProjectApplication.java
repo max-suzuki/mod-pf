de1bb48ad6f3b1617bbf3f0c2cd1dcfcace617d3
s.suzuki <s.suzuki@max-produce.com>
2020-10-06 17:17:09 +0900

    初期

---
+++ src/main/java/com/example/mod/MoviesOnDemandProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesOnDemandProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesOnDemandProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mod/MoviesOnDemandProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesOnDemandProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
